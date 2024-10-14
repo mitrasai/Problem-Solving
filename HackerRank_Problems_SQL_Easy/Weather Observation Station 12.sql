77899ff885ee48601f686efa2a23ca60c98b3e2e
Sai-Mithra <105640472+mitrasai@users.noreply.github.com>
2024-10-14 18:27:50 +0530

    Add files via upload

---
+++ HackerRank_Problems_SQL_Easy/Weather Observation Station 12.sql
@@ -0,0 +1,17 @@
+/* Weather Observation Station 12 */
+
+/* We're given an table CITY which consists of ID(Number), CITY(VARCHAR),
+   STATE(VARCHAR), LAT_N(NUMBER), LONG_W(NUMBER) 
+
+   where LAT_N is the northern latitude and LONG_W is the western longitude.
+
+   Task -> Query the list of CITY names from STATION that either do not start 
+   with vowels and do not end with vowels. 
+   Your result cannot contain duplicates.
+ */
+
+SELECT DISTINCT CITY
+FROM STATION
+WHERE LOWER(SUBSTR(CITY, 1, 1)) NOT IN ('a', 'e', 'i', 'o', 'u')
+    AND
+    LOWER(SUBSTR(CITY, LENGTH(CITY), 1)) NOT IN ('a', 'e', 'i', 'o', 'u');
---
+++ HackerRank_Problems_SQL_Easy/Weather Observation Station 9.sql
@@ -0,0 +1,18 @@
+/* Weather Observation Station 9 */
+
+/* We're given an table CITY which consists of ID(Number), CITY(VARCHAR),
+   STATE(VARCHAR), LAT_N(NUMBER), LONG_W(NUMBER) 
+
+   where LAT_N is the northern latitude and LONG_W is the western longitude.
+
+   Task -> Query the list of CITY names from STATION that do not start with vowels. 
+   Your result cannot contain duplicates.
+ */
+
+SELECT DISTINCT CITY
+FROM STATION
+WHERE NOT (CITY LIKE 'a%' OR
+      CITY LIKE 'e%' OR
+      CITY LIKE 'i%' OR
+      CITY LIKE 'o%' OR
+      CITY LIKE 'u%');
